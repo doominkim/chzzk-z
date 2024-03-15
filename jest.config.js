57b673c4f21f3d23a4f662270371b77e6830dcc9
doominkim <kimduumin@gmail.com>
2024-03-15 17:33:50 +0900

    feat. add jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: "lib",
+  testRegex: ".*\\.spec\\.ts$",
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+};
