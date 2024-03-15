21c2a7e95c1274d784a20d622a743049fb37bcc2
doominkim <kimduumin@gmail.com>
2024-03-15 17:43:28 +0900

    feat. dist upload

---
+++ dist/chzzk.d.ts
@@ -0,0 +1,6 @@
+export declare class Chzzk {
+    #private;
+    constructor();
+    addMessage(message: string): void;
+    getMessages(): any;
+}
---
+++ dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const chzzk_1 = require("./chzzk");
+const chzzk = new chzzk_1.Chzzk();
+chzzk.addMessage("Hello, world!");
+console.log(chzzk.getMessages());
+//# sourceMappingURL=test.js.map
