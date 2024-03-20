99d22b6914d349557ef72eb23a367f52b1635de4
doominkim <kimduumin@gmail.com>
2024-03-20 18:34:11 +0900

    feat. add handlers

--- lib/chzzk-connector/chzzk-connector.ts
+++ lib/chzzk-connector/chzzk-connector.ts
@@ -1,6 +1,7 @@
 import { WebSocket } from "ws";
 import { ChzzkChannel, ChzzkChat, ChzzkLive, ChzzkUser } from "./apis";
 import { ChzzkConnectorOptionDto } from "./dtos/chzzk-connector-option.dto";
+import { MsgCmd, SendMessageData } from "./types/chat.types";
 
 export class ChzzkConnector {
   private ws: WebSocket;
@@ -9,6 +10,7 @@ export class ChzzkConnector {
   chat: ChzzkChat;
   live: ChzzkLive;
   user: ChzzkUser;
+  #pingIntervalId: any;
 
   constructor() {
     this.option = new ChzzkConnectorOptionDto();
@@ -20,22 +22,72 @@ export class ChzzkConnector {
 
   async connect() {
     const ssID = Math.floor(Math.random() * 10) + 1;
-
     this.ws = new WebSocket(`wss://kr-ss${ssID}.chat.naver.com/chat`);
+    this.openHandler();
+    this.errorHandler();
+    this.closeHandler();
+    this.messageHandler();
+    this.connectHandler();
+  }
 
+  private openHandler() {
     this.ws.on("open", () => {
       console.log("connected!");
     });
+  }
 
+  private errorHandler() {
     this.ws.on("error", console.error);
+  }
 
+  private closeHandler() {
     this.ws.on("close", () => {
       console.log("disconeected");
+      this.disconnect();
     });
+  }
+  async disconnect() {
+    if (this.#pingIntervalId) {
+      clearInterval(this.#pingIntervalId);
+    }
 
+    if (this.ws.readyState === WebSocket.OPEN) {
+      this.ws.close();
+    } else {
+      console.log("already disconnected");
+    }
+  }
+
+  private messageHandler() {
     this.ws.on("message", async (data) => {
-      data = await JSON.parse(data.toString());
-      console.log(data);
+      data = JSON.parse(data.toString());
+      this.handleMessage(data);
     });
+  }
+  private async handleMessage(data: any) {
+    console.log("handleMessage => ", data);
+    switch (data["cmd"]) {
+      case MsgCmd.PING:
+        this.sendMessage({
+          cmd: MsgCmd.PONG,
+          ver: "2",
+        });
+        console.log("handleMessage => send pong");
+    }
+  }
+
+  private async sendMessage(sendMessageData: SendMessageData) {
+    this.ws.send(JSON.stringify(sendMessageData));
+  }
+
+  private connectHandler() {
+    // If the interval is reduced, overhead can occur.
+    this.#pingIntervalId = setInterval(() => {
+      this.sendMessage({
+        cmd: MsgCmd.PING,
+        ver: "2",
+      });
+      console.log("connectHandler => send ping");
+    }, 10000);
   }
 }
---
+++ lib/chzzk-connector/types/chat.types.ts
@@ -0,0 +1,53 @@
+export enum MsgCmd {
+  PING = 0,
+  PONG = 10000,
+  CONNECT = 100,
+  CONNECTED = 10100,
+  REQUEST_RECENT_CHAT = 5101,
+  RECENT_CHAT = 15101,
+  EVENT = 93006,
+  CHAT = 93101,
+  DONATION = 93102,
+  KICK = 94005,
+  BLOCK = 94006,
+  BLIND = 94008,
+  NOTICE = 94010,
+  PENALTY = 94015,
+  SEND_CHAT = 3101,
+}
+
+export enum MsgTypeCode {
+  TEXT = 1,
+  IMAGE = 2,
+  STICKER = 3,
+  VIDEO = 4,
+  RICH = 5,
+  DONATION = 10,
+  SUBSCRIPTION = 11,
+  SYSTEM_MESSAGE = 30,
+}
+
+export interface SendMessageExtras {
+  chatType: string; // "STREAMING",
+  emojis: string; // "",
+  osType: string; // "PC",
+  streamingChannelId: number;
+}
+
+export interface SendMessageBody {
+  extras: SendMessageExtras;
+  msg: string;
+  msgTime: string;
+  msgTypeCode: MsgTypeCode;
+  recentMessageCount: number;
+}
+
+export interface SendMessageData {
+  cmd: MsgCmd;
+  ver: string;
+  bdy?: SendMessageBody;
+  sid?: string;
+  tid?: number;
+  uid?: string;
+  retry?: number;
+}
