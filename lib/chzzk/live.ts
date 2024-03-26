cf633bb7861a1ad3f5ad3a78f93552842129f57e
doominkim <kimduumin@gmail.com>
2024-03-26 18:33:44 +0900

    fix. typo

--- lib/chzzk/live.ts
+++ lib/chzzk/live.ts
@@ -5,7 +5,6 @@ import { ChzzkModuleOptionDto } from "./dtos/chzzk-connector-option.dto";
 export class ChzzkLive {
   private cm: ChzzkModule;
   private opt: ChzzkModuleOptionDto;
-
   private liveRepository = new ChzzkLiveRepository();
 
   constructor(cm: ChzzkModule, opt: ChzzkModuleOptionDto) {
@@ -16,7 +15,8 @@ export class ChzzkLive {
   async findStatusByChannelId(channelId: string) {
     return await this.liveRepository.findStatusByChannelId(channelId, this.opt);
   }
+
   async findDetailByChannelId(channelId: string) {
-    return await this.liveRepository.findStatusByChannelId(channelId, this.opt);
+    return await this.liveRepository.findDetailByChannelId(channelId, this.opt);
   }
 }
